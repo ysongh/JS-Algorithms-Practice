0e46c3381bca31ec1593c3303dfe12ab735a564b
Song <yousonghou@yahoo.com>
2018-11-01 00:08:00 +0000

    add count unique values

---
+++ countUniqueValues.js
@@ -0,0 +1,19 @@
+const countUniqueValues = ar => {
+    if(ar.length === 0){
+        return 0;
+    }
+    
+    let uniqueCount = 1;
+    let currentValue = ar[0];
+    
+    for(let num of ar){
+        if(num !== currentValue){
+            uniqueCount++;
+            currentValue = num;
+        }
+    }
+    
+    return uniqueCount;
+};
+
+console.log(countUniqueValues([1, 1, 2, 2, 2, 2]));
