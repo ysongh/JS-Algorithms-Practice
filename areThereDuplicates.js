39457ce5298367d515833f5e88b8ef1d56173b7c
Song <yousonghou@yahoo.com>
2018-11-01 14:38:58 +0000

    add areThereDuplicates

---
+++ areThereDuplicates.js
@@ -0,0 +1,17 @@
+// Write a function that accepts an array and checks whether there are duplicates values in the array.
+
+const areThereDuplicates = ar => {
+    let frequencyCounter = {};
+    
+    for(let s of ar){
+        frequencyCounter[s] = (frequencyCounter[s] || 0) + 1;
+        
+        if(frequencyCounter[s] > 1){
+            return true;
+        }
+    }
+    
+    return false;
+};
+
+console.log(areThereDuplicates(['a', 'b', 'a']));
