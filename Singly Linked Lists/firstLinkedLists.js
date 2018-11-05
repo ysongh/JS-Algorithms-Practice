50d441fd8f32a3088968cb27e0f9d4df17230e43
Song <yousonghou@yahoo.com>
2018-11-05 04:49:20 +0000

    add firstLinkedLists

---
+++ Singly Linked Lists/firstLinkedLists.js
@@ -0,0 +1,37 @@
+// Write a singly linked lists that has a push method.
+
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+    push(val){
+        let newNode = new Node(val);
+        
+        if(this.head === null){
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else{
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        
+        this.length++;
+    }
+}
+
+var list = new SinglyLinkedList();
+list.push("HELLO");
+list.push("GOODBYE");
+list.push("See You Next Time");
+
+console.log(list);
