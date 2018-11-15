ad23c17256b4d0c41f1c86b312821023900711a2
Song <yousonghou@yahoo.com>
2018-11-15 17:02:11 +0000

    add remove method to firstDoubeLinkedLists

--- Doubly Linked Lists/firstDoubeLinkedLists.js
+++ Doubly Linked Lists/firstDoubeLinkedLists.js
@@ -139,6 +139,25 @@ class DoublyLinkedList{
         return true;
     }
     
+    // Remove a node in the list by a certain position
+    remove(index){
+        if(index < 0 || index >= this.length){
+            return undefined;
+        }
+        if(index === 0){
+            return this.shift();
+        }
+        if(index === this.length - 1){
+            return this.pop();
+        }
+        let removedNode = this.get(index);
+        removedNode.prev.next = removedNode.next;
+        removedNode.next.prev = removedNode.prev;
+        removedNode.next = null;
+        removedNode.prev = null;
+        this.length--;
+        return removedNode;
+    }
 }
 
 let list = new DoublyLinkedList();
