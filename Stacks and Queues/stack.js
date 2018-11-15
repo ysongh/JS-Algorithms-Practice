5c60f1e61ae376e499e2312a214e5459bd083c1f
Song <yousonghou@yahoo.com>
2018-11-15 04:26:22 +0000

    add push method to stack

--- Stacks and Queues/stack.js
+++ Stacks and Queues/stack.js
@@ -11,6 +11,22 @@ class Stack{
         this.last = null;
         this.size = 0;
     }
+    
+    // Add a new node at the beginning of the stack
+    push(val){
+        let newNode = new Node(val);
+        if(this.size === 0){
+            this.first = newNode;
+            this.last = newNode;
+        }
+        else{
+            let temp = this.first;
+            this.first = newNode;
+            this.first.next = temp;
+        }
+        this.size++;
+        return this.size;
+    }
 }
 
 let list = new Stack();
