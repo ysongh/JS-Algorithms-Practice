47157d8f950d4a4d3a963c0163d310e51399a9de
Song <yousonghou@yahoo.com>
2018-11-15 04:06:19 +0000

    setup stack class

---
+++ Stacks and Queues/stack.js
@@ -0,0 +1,17 @@
+class Node{
+    constructor(val){
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class Stack{
+    constructor(){
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+}
+
+let list = new Stack();
+console.log(list);
