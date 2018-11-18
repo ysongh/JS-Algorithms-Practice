606e27e4f784d9fc84e9215f2a1642739ca88341
Song <yousonghou@yahoo.com>
2018-11-18 19:29:35 +0000

    add insert method to BinarySearchTree

--- Tree/BinarySearchTree.js
+++ Tree/BinarySearchTree.js
@@ -10,6 +10,41 @@ class BinarySearchTree{
     constructor(){
         this.root = null;
     }
+    
+    // add a new node into the tree
+    insert(value){
+        let newNode = new Node(value);
+        if(this.root === null){
+            this.root = newNode;
+            return this;
+        }
+        else{
+            let current = this.root;
+            while(true){
+                if(value > current.value){
+                    if(current.right === null){
+                        current.right = newNode;
+                        return this;
+                    }
+                    else{
+                        current = current.right;
+                    }
+                }
+                else if(value < current.value){
+                    if(current.left === null){
+                        current.left = newNode;
+                        return this;
+                    }
+                    else{
+                        current = current.left;
+                    }
+                }
+                else{
+                    return undefined;
+                }
+            }
+        }
+    }
 }
 
 const bst = new BinarySearchTree();
