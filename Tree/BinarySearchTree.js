cd96b5ca83c47b58c3a68dd6d77855dd5989d155
Song <yousonghou@yahoo.com>
2018-11-18 06:08:31 +0000

    setup binary search tree

---
+++ Tree/BinarySearchTree.js
@@ -0,0 +1,16 @@
+class Node{
+    constructor(value){
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree{
+    constructor(){
+        this.root = null;
+    }
+}
+
+const bst = new BinarySearchTree();
+console.log(bst);
