b13e6d68500200aae45e4667301033a191203e42
Song <yousonghou@yahoo.com>
2018-11-30 17:44:57 +0000

    add extractMax method to Max Binary Heap

--- Tree/maxBinaryHeap.js
+++ Tree/maxBinaryHeap.js
@@ -25,6 +25,58 @@ class MaxBinaryHeap{
             index = parentIndex;
         }
     }
+    
+    // remove the root and replace it with the last node or element.
+    extractMax(){
+        const oldRoot = this.values[0];
+        const lastNode = this.values.pop();
+        
+        if(this.values.length > 0){
+            this.values[0] = lastNode;
+            this.bubbleDown();
+        }
+        
+        return oldRoot;
+    }
+    
+    // move the value down if the parent value is less than it child value.
+    bubbleDown(){
+        let parentIndex = 0;
+        let parentValue = this.values[0];
+        const len = this.values.length;
+        
+        while(true){
+            let leftChildIndex = 2 * parentIndex + 1;
+            let rightChildIndex = 2 * parentIndex + 2;
+            let leftChild;
+            let rightChild;
+            let swap = null;
+            
+            if(leftChildIndex < len){
+                leftChild = this.values[leftChildIndex];
+                if(leftChild > this.values[parentIndex]){
+                    swap = leftChildIndex;
+                }
+            }
+            
+            if(rightChildIndex < len){
+                rightChild = this.values[rightChildIndex];
+                if((swap === null && rightChild > this.values[parentIndex]) ||
+                    (swap !== null && rightChild > leftChild)){
+                    swap = rightChildIndex;
+                }
+            }
+            
+            if(swap === null){
+                break;
+            }
+            
+            this.values[parentIndex] = this.values[swap];
+            this.values[swap] = parentValue;
+            parentIndex = swap;
+        }
+    }
+    
 }
 
 let heap = new MaxBinaryHeap();
