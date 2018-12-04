8c8be985d79316725b08c3b4e4efca4c3deb38e9
Song <yousonghou@yahoo.com>
2018-12-04 15:53:58 +0000

    fix binarySearch.js

--- Search/BinarySearch.js
+++ Search/BinarySearch.js
@@ -1,20 +1,23 @@
 const binarySearch = (arr, target) => {
     let left = 0;
-    let right = arr.length;
+    let right = arr.length - 1;
+    let middle = Math.floor((left + right) / 2);
     
-    while(left < right){
-        let middle = Math.floor(right / 2);
+    while(left <= right){
+        
         if(arr[middle] === target){
             return middle;
         }
         else if(arr[middle] < target){
-            left = middle;
+            left = middle + 1;
         }
         else{
-            right = middle;
+            right = middle - 1;
         }
+        
+        middle = Math.floor((left + right) / 2);
     }
     return -1;
 };
 
-console.log(binarySearch([1, 5, 8, 9], 8));
+console.log(binarySearch([1, 5, 8, 9], 5));
