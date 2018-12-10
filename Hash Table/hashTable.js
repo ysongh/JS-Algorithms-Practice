f59a794ae6a19e1d232acb9c5f3cec9a8ca08b2d
Song <yousonghou@yahoo.com>
2018-12-10 05:02:44 +0000

    add set method to hash table

--- Hash Table/hashTable.js
+++ Hash Table/hashTable.js
@@ -1,6 +1,6 @@
 // Note - this class only works on string
 class HashTable{
-    constructor(size=53){
+    constructor(size=57){
         this.keyMap = new Array(size);
     }
     
@@ -9,9 +9,23 @@ class HashTable{
         let primeNumber = 31;
         for(let i = 0; i < Math.min(key.length, 100); i++){
             let char = key[i];
-            let value = char.charCodeAt(0) - 96;
+            let value = char.charCodeAt(0);
             total = (total * primeNumber + value) % this.keyMap.length;
         }
         return total;
     }
+    
+    set(key, value){
+        let index = this._hash(key);
+        if(!this.keyMap[index]){
+            this.keyMap[index] = [];
+        }
+        this.keyMap[index].push([key, value]);
+        return index;
+    }
 }
+
+let ht = new HashTable();
+console.log(ht.set("cools", "tree"));
+console.log(ht.set("test", "plan"));
+console.log(ht.set("candy", "chips"));
