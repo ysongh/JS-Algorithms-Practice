c7b477fb76099c774861ab54ce89e81baf9467eb
Song <yousonghou@yahoo.com>
2018-12-23 04:20:59 +0000

    add addEdge method to graph

--- Graph/adjacencyList.js
+++ Graph/adjacencyList.js
@@ -11,7 +11,16 @@ class Graph{
             this.adjacencyList[vertex] = [];
         }
     }
+    
+    // add connection between vertices
+    addEdge(vertex1, vertex2){
+        this.adjacencyList[vertex1].push(vertex2);
+        this.adjacencyList[vertex2].push(vertex1);
+    }
 }
 
 const g = new Graph();
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("C");
 console.log(g);
