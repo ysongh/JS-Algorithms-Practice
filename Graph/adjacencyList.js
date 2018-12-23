fca359780c230f67abd5d4ab3f70ab12303663a5
Song <yousonghou@yahoo.com>
2018-12-23 05:24:24 +0000

    add removeEdge method to graph

--- Graph/adjacencyList.js
+++ Graph/adjacencyList.js
@@ -17,6 +17,16 @@ class Graph{
         this.adjacencyList[vertex1].push(vertex2);
         this.adjacencyList[vertex2].push(vertex1);
     }
+    
+    //remove a vertex from adjacencyList
+    removeEdge(vertex1, vertex2){
+        this.adjacencyList[vertex1] = this.adjacencyList[vertex1].filter(
+            vertex => vertex !== vertex2
+        );
+        this.adjacencyList[vertex2] = this.adjacencyList[vertex2].filter(
+            vertex => vertex !== vertex1
+        );
+    }
 }
 
 const g = new Graph();
