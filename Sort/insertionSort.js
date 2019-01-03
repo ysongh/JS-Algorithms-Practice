26d3d9f684280b60d16f7d7bc0cb35928cdf6913
Song <yousonghou@yahoo.com>
2019-01-03 04:51:52 +0000

    make changes to insertion sort

--- Sort/insertionSort.js
+++ Sort/insertionSort.js
@@ -2,16 +2,16 @@ const insertionSort = arr => {
     const len = arr.length;
     
     for(let i = 1; i < len; i++){
-        for(let j = i; j > 0; j--){
-            if(arr[j] < arr[j - 1]){
-                const temp = arr[j];
-                arr[j] = arr[j - 1];
-                arr[j - 1] = temp;
-            }
+        let j = i;
+        while(arr[j] < arr[j - 1] && j - 1 >= 0){
+            const temp = arr[j];
+            arr[j] = arr[j - 1];
+            arr[j - 1] = temp;
+            j--;
         }
     }
     
     return arr;
 };
 
-console.log(insertionSort([3, 9, 14, 1, 7]));
+console.log(insertionSort([9, 3, 14, 1, 7]));
