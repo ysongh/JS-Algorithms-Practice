a89f08392ac58b03a62e63ba7a6a697e029de07b
Song <yousonghou@yahoo.com>
2019-01-26 03:33:52 +0000

    add addEdge method to weighted graph

--- Graph/Weighted Graph/adjacencyList.js
+++ Graph/Weighted Graph/adjacencyList.js
@@ -9,6 +9,12 @@ class WeightedGraph{
             this.adjacencyList[vertex] = [];
         }
     }
+    
+    // add a edge between two vertexs with weight to adjacencyList
+    addEdge(vertex1, vertex2, weight){
+        this.adjacencyList[vertex1].push({node: vertex2, weight: weight});
+        this.adjacencyList[vertex2].push({node: vertex1, weight: weight});
+    }
 }
 
 const g = new WeightedGraph();
